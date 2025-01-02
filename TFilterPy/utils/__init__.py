d03846194365181d7a00bf66d23502d1a7066635
thabang sekgoto <realthabangvision@gmail.com>
2025-01-02 23:24:02 +0200

    adding all the filess

---
+++ TFilterPy/utils/__init__.py
@@ -0,0 +1,8 @@
+"""
+TFilterPy: A Python package for state estimation, including Kalman Filters,
+Particle Filters, and Nonlinear Filters.
+"""
+
+from .optimisation_utils import ParameterEstimator
+
+__version__ = "0.0.1"
